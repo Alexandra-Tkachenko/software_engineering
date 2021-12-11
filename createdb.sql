65b55116d3e17c9b0eacd6b95f2ae516e1ee380d
Alexandra-Tkachenko <91024312+Alexandra-Tkachenko@users.noreply.github.com>
2021-12-11 19:53:46 +0300

    Update createdb.sql

--- createdb.sql
+++ createdb.sql
@@ -21,16 +21,18 @@ create table expense(
 
 insert into category (codename, name, is_base_expense, aliases)
 values
-    ("products", "продукты", true, "еда"),
+    ("products", "продукты", true, "еда, провизия, food, пища, продовольствие, ням-ням"),
     ("coffee", "кофе", true, ""),
-    ("dinner", "обед", true, "столовая, ланч, бизнес-ланч, бизнес ланч"),
-    ("cafe", "кафе", true, "ресторан, рест, мак, макдональдс, макдак, kfc, ilpatio, il patio"),
-    ("transport", "общ. транспорт", false, "метро, автобус, metro"),
-    ("taxi", "такси", false, "яндекс такси, yandex taxi"),
-    ("phone", "телефон", false, "теле2, связь"),
-    ("books", "книги", false, "литература, литра, лит-ра"),
-    ("internet", "интернет", false, "инет, inet"),
-    ("subscriptions", "подписки", false, "подписка"),
+    ("dinner", "обед", true, "столовая, ланч, бизнес-ланч, перекус"),
+    ("cafe", "кафе", true, "ресторан, фастфуд"),
+    ("transport", "общ. транспорт", false, "метро, автобус, metro, subway, самолет, самокат, велосипед"),
+    ("taxi", "такси", false, "яндекс такси, yandex taxi, ситимобил, таксичкофф"),
+    ("phone", "телефон", false, "мобильный, оператор, связь"),
+    ("books", "книги", false, "литература, просвещение, образование"),
+    ("internet", "интернет", false, "инет, inet, вай-фай, wi-fi"),
+    ("subscriptions", "подписки", false, "подписка, yandex, kinopoisk, кинопоиск, музыка, youtube"),
+    ("courses", "курсы", false, "хекслет, яндекс практикум, курсера, coursera, udemy");
+    ("programms", "программы", false, "фотошоп, 3Dcoat, zbrush, substain, marvelousDesigner, clipStudioPaint, illustrator, marmoset, clion, pycharm");
     ("other", "прочее", true, "");
 
 insert into budget(codename, daily_limit) values ('base', 500);
